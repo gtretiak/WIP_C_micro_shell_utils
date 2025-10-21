f924e72939fd01044fb3603203198da18eae8953
Georgii Tretiakov <gtretiak@c2r4s4.42porto.com>
2025-10-21 11:06:21 +0100

    picoshell

--- picoshell.c
+++ picoshell.c
@@ -17,9 +17,8 @@ arguments of each command is the command name or path and can be passed
 directly as the first argument of execvp.
 
 If any error occur, the function must return 1 (you must of course
-close all the open fds before). otherwise the function must wait all child
-processes and return 0. You will find in this directory a file main.c which
-contain something to help you test your function.
+close all the open fds before). Otherwise the function must wait all child
+processes and return 0. 
 
 Examples: 
 ./picoshell /bin/ls "|" /usr/bin/grep picoshell
@@ -30,35 +29,94 @@ squblblb/
 
 #include <unistd.h>
 #include <sys/wait.h>
+//#include <sys/types.h>
+#include <stdlib.h>
 
 int	picoshell(char **cmds[])
 {
 	int	i = 0;
 	int	fd[2];
+	int	fd_to_use = 0;
+	int	last_ret;
+	int	total_ret = 0;
 	pid_t	pid;
-	while (cmds[i])
+	while (cmds[i]) // while cmds
 	{
-		if (pipe(fd) < 0)
-			exit(1);
+		if (cmds[i + 1]) // if it isn't the last
+		{
+			if (pipe(fd) < 0)
+			{
+				if (fd_to_use != 0)
+					close(fd_to_use);
+				return (1);
+			}
+		}
+		else // otherwise we don't need a pipe
+		{
+			fd[0] = -1;
+			fd[1] = -1;
+		}
 		pid = fork();
 		if (pid < 0)
-			exit(1);
+		{
+			if (fd[0] != -1 || fd[1] != -1)
+			{
+				close(fd[0]);
+				close(fd[1]);
+			}
+			if (fd_to_use != 0)
+				close(fd_to_use);
+			return (1);
+		}
 		if (pid == 0)
-		{}
-
+		{
+			if (fd_to_use != 0) // not the first cmd, so we need previous output
+			{
+				if (dup2(fd_to_use, 0) < 0)
+					exit(1);
+				close(fd_to_use);
+			}
+			if (fd[1] != -1) // not the last cmd, so we need to redirect
+			{
+				if (dup2(fd[1], 1) < 0)
+					exit(1);
+				close(fd[1]);
+				close(fd[0]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
+		}
+		if (fd[1] != -1)
+			close(fd[1]);
+		if (fd_to_use != 0)
+			close(fd_to_use);
+		fd_to_use = fd[0];
 		i++;
 	}
+	while (wait(&last_ret) > 0)
+	{
+		if (WIFEXITED(last_ret) && WEXITSTATUS(last_ret) != 0)
+			total_ret = 1;
+		else if (!WIFEXITED(last_ret))
+			total_ret = 1;
+	}
+	return (total_ret);
 }
 
 int main(void)
 {
 	char **cmds[4];
-	//static char *cmd1[] = {"cat", NULL};
-	//static char *cmd2[] = {"cat", NULL};
-	//static char *cmd3[] = {"ls", "-l", NULL};
 	static char *cmd1[] = {"ls", NULL};
 	static char *cmd2[] = {"grep", "l", NULL};
 	static char *cmd3[] = {"wc", NULL};
+	/* Another test.
+	 * This time ls doesn't require input
+	 * This means that if cat cmds aren't ended with Ctrl+D (EoF), they send text to ls
+	 * which leads to error.
+	static char *cmd1[] = {"cat", NULL};
+	static char *cmd2[] = {"cat", NULL};
+	static char *cmd3[] = {"ls", "-l", NULL};
+	*/
 	cmds[0] = cmd1;
 	cmds[1] = cmd2;
 	cmds[2] = cmd3;
